62c208c02c5774abd7bd16e18635645ae4c4b0e0
Zaktansnake <david_chongym@outlook.com>
2015-09-19 04:47:17 +0800

    Formatting the TextBuddy program description
    Formatting the TextBuddy program description that informs viewers about
    the TextBuddy programs and its usage.
    
    [Reason: Personal Organization]

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -5,21 +5,20 @@
  */
 
 /**
-	 * TextBuddy is a program that users can use to add, delete, search, display, clear and sort text.
-	 * Users can perform these functions by typing the commands in the command line
-	 * These are the proper usage of the commands,
-	 * To open the program, java TextBuddy filename.txt
-	 * For addding a line of text, add userinput
-	 * For deleting a line of text, delete linenumber
-	 * For searching for a specific word, search userinput
-	 * For displaying all the text, display
-	 * For clearing all the text, clear
-	 * For sorting text according to alphabetical order, sort
-	 * 
-	 * The file that stores the text is auto-saved after each action so it is always updated
-	 * 
-	 * @author David Chong
-	 */ 
+* TextBuddy is a program that users can use to add, delete, search, display, clear and sort text.
+* Users can perform these functions by typing the commands in the command line
+* These are the proper usage of the commands,
+* To open the program, java TextBuddy filename.txt
+* For addding a line of text, add userinput
+* For deleting a line of text, delete linenumber
+* For searching for a specific word, search userinput
+* For displaying all the text, display
+* For clearing all the text, clear
+* For sorting text according to alphabetical order, sort
+* 
+* The file that stores the text is auto-saved after each action so it is always updated
+* @author David Chong
+*/ 
 
 import java.util.*;
 import java.io.*;
